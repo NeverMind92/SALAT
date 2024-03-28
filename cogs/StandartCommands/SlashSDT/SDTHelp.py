8399f558209aef34f3ea8a8f8ffdf3cae038db5b
NeverMind92 <llegionov2@gmail.com>
2024-03-28 19:39:12 +0400

    micro fix

--- cogs/StandartCommands/SlashSDT/SDTHelp.py
+++ cogs/StandartCommands/SlashSDT/SDTHelp.py
@@ -3,20 +3,19 @@ from disnake.ext import commands
 import datetime
 import config
 
-class Ban(commands.Cog):
+class Help(commands.Cog):
     def __init__ (self, bot: commands.Bot):
         self.bot = bot
-        
-    @commands.has_permissions(ban_members=True)
-    @commands.slash_command(name="ban", description="bans a member who broke the rules")
-    async def ban(self, ctx, member: disnake.Member, *, reason):
-        embed=disnake.Embed(title=":white_check_mark: Ban",description=f"{member.mention} has been banned for **{reason}**", color=0x006400, timestamp=datetime.datetime.now())
-        embed.add_field(name="By Admin/moderator:", value=f"{ctx.author.mention}", inline=None)
+
+    @commands.command()
+    async def help(self, ctx):
+        embed = disnake.Embed(title="Help", description="All commands in Salat", color=0x006400, timestamp=datetime.datetime.now())
+        embed.add_field(name=":page_facing_up: Info", value="".join(config.info_commands), inline=False)
+        embed.add_field(name=":hammer: Moderation", value="".join(config.moderation_commands), inline=False)
+        embed.add_field(name=":toolbox: Tools", value="".join(config.tools_commands), inline=False)
         embed.set_footer(text="SALAT!", icon_url="https://cdn.discordapp.com/app-icons/1215958585314381824/b9d4ec6f686085758ad2e7fdc5503f0d.png?size=256")
         await ctx.send(embed=embed)
-        await ctx.guild.ban(member, reason=reason)
-
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(Ban(bot))
+    bot.add_cog(Help(bot))
