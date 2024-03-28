f648c1947d970ca06d79cfffa834e6086eb2cf8c
NeverMind92 <llegionov2@gmail.com>
2024-03-28 16:55:41 +0400

    add Error handler

--- bot.py
+++ bot.py
@@ -10,6 +10,26 @@ bot = commands.Bot(command_prefix='?',intents=intents, sync_commands_debug=True)
 bot.load_extensions('cogs/StandartCommands')
 bot.load_extensions('cogs/ModerationCommands')
 
+@bot.event
+async def on_command_error(inter: disnake.ApplicationCommandInteraction, error):
+  if isinstance(error, commands.MissingPermissions):
+    #Instead of commands.Missing Permissions you insert your error
+    embed = disnake.Embed(
+        title='Missing permissions!',
+        description='You dont have any Admin/moderator permisssions!',
+        color=disnake.Color.red)
+    await inter.send(embed=embed, ephemeral=True)
+
+@bot.event
+async def on_slash_command_error(inter: disnake.ApplicationCommandInteraction, error):
+  if isinstance(error, commands.MissingPermissions):
+    #Instead of commands.Missing Permissions you insert your error
+    embed = disnake.Embed(
+        title='Missing permissions!',
+        description='You dont have any Admin/moderator permisssions!',
+        color=disnake.Color.red)
+    await inter.send(embed=embed, ephemeral=True)
+
 
 @bot.event
 async def on_ready():
