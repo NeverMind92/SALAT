28314d94c6e924757fb0fb8962fb071cc9b8591a
NeverMind92 <llegionov2@gmail.com>
2024-03-28 16:47:56 +0400

    Unban added

---
+++ cogs/ModerationCommands/Unban.py
@@ -0,0 +1,33 @@
+import disnake
+from disnake.ext import commands
+import datetime
+import config
+
+class Unban(commands.Cog):
+    def __init__ (self, bot: commands.Bot):
+        self.bot = bot
+    
+    @commands.has_permissions(ban_members=True)
+    @commands.command()
+    async def unban(self, ctx, member: disnake.Member, *, reason=None):
+        embed=disnake.Embed(title=":white_check_mark: Unban",description=f"{member.mention} has been unbanned", color=0x006400, timestamp=datetime.datetime.now())
+        embed.add_field(name="By Admin/moderator:", value=f"{ctx.author.mention}", inline=None)
+        embed.set_footer(text="SALAT!", icon_url="https://cdn.discordapp.com/app-icons/1215958585314381824/b9d4ec6f686085758ad2e7fdc5503f0d.png?size=256")
+        await ctx.send(embed=embed)
+        await ctx.guild.unban(member, reason=reason)
+ 
+
+
+    @commands.has_permissions(ban_members=True)
+    @commands.slash_command()
+    async def unban(self, ctx, member: disnake.Member, *, reason):
+        embed=disnake.Embed(title=":white_check_mark: Unban",description=f"{member.mention} has been unbanned", color=0x006400, timestamp=datetime.datetime.now())
+        embed.add_field(name="By Admin/moderator:", value=f"{ctx.author.mention}", inline=None)
+        embed.set_footer(text="SALAT!", icon_url="https://cdn.discordapp.com/app-icons/1215958585314381824/b9d4ec6f686085758ad2e7fdc5503f0d.png?size=256")
+        await ctx.send(embed=embed)
+        await ctx.guild.unban(member, reason=reason)
+
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Unban(bot))
