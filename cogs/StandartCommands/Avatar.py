2d64881a99804a06cd13ff79a0dbb0d9de975143
NeverMind92 <llegionov2@gmail.com>
2024-03-30 14:00:35 +0400

    fix Avatar

--- cogs/StandartCommands/Avatar.py
+++ cogs/StandartCommands/Avatar.py
@@ -8,9 +8,12 @@ class Msg_avatar(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def avatar(self, ctx, member: disnake.Member):
-        embed=disnake.Embed(title="Avatar", color=disnake.Color.dark_green)
-        embed.set_image(url=member.display_avatar.url)
+    async def avatar(ctx, member: disnake.Member = None):
+        if member is None:
+            member = ctx.author
+        embed = disnake.Embed(title=f"{member.name}'s Avatar", color=disnake.Color.dark_green(), timestamp=datetime.datetime.now())
+        embed.set_footer(text="SALAT!", icon_url="https://cdn.discordapp.com/app-icons/1215958585314381824/b9d4ec6f686085758ad2e7fdc5503f0d.png?size=256")
+        embed.set_image(url=member.avatar.url)
         await ctx.send(embed=embed)
 
 
--- cogs/StandartCommands/SlashSDT/SDTAvatar.py
+++ cogs/StandartCommands/SlashSDT/SDTAvatar.py
@@ -8,12 +8,13 @@ class Avatar(commands.Cog):
         self.bot = bot
 
     @commands.slash_command()
-    async def avatar(self, ctx, member: disnake.Member):
-        user = member or ctx.author
-        embed=disnake.Embed(title="Avatar", color=disnake.Color.dark_green)
-        embed.set_image(url=member.display_avatar.url)
+    async def avatar(ctx, member: disnake.Member = None):
+        if member is None:
+            member = ctx.author
+        embed = disnake.Embed(title=f"{member.name}'s Avatar", color=disnake.Color.dark_green(), timestamp=datetime.datetime.now())
+        embed.set_footer(text="SALAT!", icon_url="https://cdn.discordapp.com/app-icons/1215958585314381824/b9d4ec6f686085758ad2e7fdc5503f0d.png?size=256")
+        embed.set_image(url=member.avatar.url)
         await ctx.send(embed=embed)
-
 
 def setup(bot: commands.Bot):
     bot.add_cog(Avatar(bot))
